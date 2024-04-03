78c295d55dbb178363d821caee448f849ff22054
Flynn <wphestiraid@gmail.com>
2024-04-03 20:03:56 +0900

    7.2. DocumentGPT: Data Flow

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+
 import streamlit as st
+
+today = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+st.title(today)
 
 st.write("Hello, world!")
 
@@ -6,4 +11,11 @@ st.write([1, 2, 3])
 
 st.write({"key": "value"})
 
-st.selectbox("Choose a number", ("GPT-3", "GPT-4"))
+model = st.selectbox("Choose a number", ("GPT-3", "GPT-4"))
+
+st.write(f"Selected model: {model}")
+
+
+name = st.text_input("Enter your name", "John Doe")
+
+st.write(f"Hello, {name}!")
