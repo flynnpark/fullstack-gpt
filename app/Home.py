2fb937b759644f04bd8aa923730aaa4dc9a212db
Flynn <wphestiraid@gmail.com>
2024-04-05 09:17:03 +0900

    7.4. Chat Messages

--- app/Home.py
+++ app/Home.py
@@ -1,26 +1,14 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="FullstackGPT Home",
-    page_icon="🧊",
+st.markdown(
+    """
+# Hello!
+
+Welcome to the **GPT** app! 📄✒️
+
+Here are the apps I made:
+
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [QuizGPT](/QuizGPT)
+"""
 )
-
-st.title("FullstackGPT")
-
-with st.sidebar:
-    st.title("Sidebar")
-    st.text_input("Enter your name")
-
-tab_one, tab_two, tab_three = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
-
-with tab_one:
-    st.title("Tab 1")
-    st.write("This is tab 1")
-
-with tab_two:
-    st.title("Tab 2")
-    st.write("This is tab 2")
-
-with tab_three:
-    st.title("Tab 3")
-    st.write("This is tab 3")
--- app/pages/DocumentGPT.py
+++ app/pages/DocumentGPT.py
@@ -1,3 +1,33 @@
+import time
+
 import streamlit as st
 
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📄",
+    layout="centered",
+    initial_sidebar_state="auto",
+)
 st.title("DocumentGPT")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+session_messages = st.session_state["messages"]
+
+
+def send_message(message: str, role: str, save: bool = True):
+    with st.chat_message(role):
+        st.write(message)
+        if save:
+            session_messages.append({"role": role, "message": message})
+
+
+for message in session_messages:
+    send_message(message["message"], message["role"], save=False)
+
+message = st.chat_input("Send message to the AI")
+if message:
+    send_message(message, "human")
+    time.sleep(2)
+    send_message("I am a robot 🤖", "ai")
